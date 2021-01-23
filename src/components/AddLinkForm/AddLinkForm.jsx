ae50000c29396ff463a73799314e54afc612a248
cnbilgin <cnbilgin@hotmail.com>
2021-01-23 11:37:14 +0300

    create addlinkform

---
+++ src/components/AddLinkForm/AddLinkForm.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import classes from "./AddLinkForm.module.css";
+import Button from "../Button/Button";
+import { useHistory } from "react-router-dom";
+
+export default function AddLinkForm() {
+	const [data, setData] = useState({
+		name: "",
+		link: "",
+	});
+
+	const history = useHistory();
+
+	const handleOnChange = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
+
+		const newData = { ...data };
+		newData[name] = value;
+
+		setData(newData);
+	};
+
+	const redirect = () => {
+		history.push("/");
+	};
+
+	const handleAdd = () => {
+		let isValid = true;
+		const { name, link } = data;
+		let errMessage = null;
+
+		if (!(isValid = isValid && name.length > 0))
+			errMessage = "Please type a name";
+		else if (!(isValid = isValid && link.length > 0))
+			errMessage = "Please type a link";
+		else if (
+			!(isValid = isValid && /^(ftp|http|https):\/\/[^ "]+$/.test(link))
+		)
+			errMessage = "Please type a valid link";
+
+		if (errMessage) alert(errMessage);
+		else redirect();
+	};
+
+	return (
+		<div className={classes.addForm}>
+			<div className={classes.formItem}>
+				<label>Link Name:</label>
+				<input
+					name="name"
+					type="text"
+					placeholder="e.g. Alphabet"
+					onChange={handleOnChange}
+					value={data.name}
+				/>
+			</div>
+			<div className={classes.formItem}>
+				<label>Link URL:</label>
+				<input
+					name="link"
+					type="text"
+					placeholder="e.g. http://abc.xyz"
+					onChange={handleOnChange}
+					value={data.link}
+				/>
+			</div>
+			<div className={classes.buttons}>
+				<Button onClick={handleAdd}>ADD</Button>
+			</div>
+		</div>
+	);
+}
--- src/containers/LinkAddContainer/LinkAddContainer.jsx
+++ src/containers/LinkAddContainer/LinkAddContainer.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import classes from "./LinkAddContainer.module.css";
+import { FaLongArrowAltLeft } from "react-icons/fa";
+import AddLinkForm from "../../components/AddLinkForm/AddLinkForm";
 
 export default function LinkAddContainer() {
-	return <div></div>;
+	return (
+		<div className={classes.linkAddContainer}>
+			<Link to="/" className={classes.returnLink}>
+				<FaLongArrowAltLeft />
+				Return to List
+			</Link>
+			<h1 className={classes.title}>Add New Link</h1>
+			<AddLinkForm />
+		</div>
+	);
 }
