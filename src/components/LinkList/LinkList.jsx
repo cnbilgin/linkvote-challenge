15c94cd9095f460c3667489f01f2ba2680718500
cnbilgin <cnbilgin@hotmail.com>
2021-01-24 16:16:44 +0300

    tests for linklist

--- src/components/LinkList/LinkList.jsx
+++ src/components/LinkList/LinkList.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import LinkListItem from "./LinkListItem";
+import classes from "./LinkList.module.css";
 
 export default function LinkList({ links, onVote, onRemove }) {
 	return (
-		<div>
-			{links.map((link) => (
-				<LinkListItem
-					onVote={onVote}
-					key={link.id}
-					onRemove={onRemove}
-					link={link}
-				/>
-			))}
+		<div data-testid="cont">
+			{links && links.length > 0 ? (
+				links.map((link) => (
+					<LinkListItem
+						onVote={onVote}
+						key={link.id}
+						onRemove={onRemove}
+						link={link}
+					/>
+				))
+			) : (
+				<div className={classes.emptyWarning}>There is no link in list</div>
+			)}
 		</div>
 	);
 }
---
+++ src/components/LinkList/LinkList.test.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { fireEvent, logRoles, render, screen } from "@testing-library/react";
+import LinkList from "./LinkList";
+
+export const linkData = [
+	{
+		points: 3,
+		name: "Test Page",
+		link: "http://test.com",
+		id: "abcde1234",
+	},
+	{
+		points: 5,
+		name: "Test2 Page",
+		link: "http://test2.com",
+		id: "abcde1235",
+	},
+	{
+		points: 1,
+		name: "Test3 Page",
+		link: "http://test3.com",
+		id: "abcde1236",
+	},
+];
+
+describe("LinkList", () => {
+	test("renders", () => {
+		render(<LinkList links={linkData} />);
+
+		expect(screen.getByTestId("cont").children.length).toBe(linkData.length);
+	});
+
+	test("renders with different amount of data", () => {
+		render(<LinkList links={linkData.slice(0, 2)} />);
+
+		expect(screen.getByTestId("cont").children.length).toBe(2);
+	});
+
+	test("renders with zero data", () => {
+		render(<LinkList links={[]} />);
+
+		expect(screen.getByText("There is no link in list")).toBeInTheDocument();
+	});
+
+	test("renders with no data", () => {
+		render(<LinkList />);
+
+		expect(screen.getByText("There is no link in list")).toBeInTheDocument();
+	});
+});
