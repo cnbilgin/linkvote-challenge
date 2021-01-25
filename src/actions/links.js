9c35ff16a67efce0517971642f59fbbfaa4c6d0f
cnbilgin <cnbilgin@hotmail.com>
2021-01-25 11:23:36 +0300

    change action creators to arrow function for clean look

--- src/actions/links.js
+++ src/actions/links.js
@@ -8,109 +8,79 @@ export const DOWNVOTE_LINK = "DOWNVOTE_LINK";
 export const LOAD_LINKS = "LOAD_LINKS";
 export const ORDER_LINKS = "ORDER_LINKS";
 
-function addLink(link) {
-	return {
-		type: ADD_LINK,
-		link,
-	};
-}
+const addLink = (link) => ({
+	type: ADD_LINK,
+	link,
+});
 
-function removeLink(id) {
-	return {
-		type: REMOVE_LINK,
-		id,
-	};
-}
+const removeLink = (id) => ({
+	type: REMOVE_LINK,
+	id,
+});
 
-function upvoteLink(id) {
-	return {
-		type: UPVOTE_LINK,
-		id,
-	};
-}
+const upvoteLink = (id) => ({
+	type: UPVOTE_LINK,
+	id,
+});
 
-function downvoteLink(id) {
-	return {
-		type: DOWNVOTE_LINK,
-		id,
-	};
-}
+const downvoteLink = (id) => ({
+	type: DOWNVOTE_LINK,
+	id,
+});
 
-function loadLinks(links) {
-	return {
-		type: LOAD_LINKS,
-		links,
-	};
-}
+const loadLinks = (links) => ({
+	type: LOAD_LINKS,
+	links,
+});
 
-function orderLinks(links) {
-	return {
-		type: ORDER_LINKS,
-		links,
-	};
-}
+const orderLinks = (links) => ({
+	type: ORDER_LINKS,
+	links,
+});
 
-export function handleOrderLinks(orderBy) {
-	return (dispatch) => {
-		return linkService.get().then((links) => {
-			let compareValue = 1;
-			if (orderBy === orderTypes.ASC) compareValue = -1;
-			else if (orderBy === orderTypes.DESC) compareValue = 1;
+export const handleOrderLinks = (orderBy) => (dispatch) =>
+	linkService.get().then((links) => {
+		let compareValue = 1;
+		if (orderBy === orderTypes.ASC) compareValue = -1;
+		else if (orderBy === orderTypes.DESC) compareValue = 1;
 
-			links.reverse().sort((a, b) => {
-				if (b.points > a.points) return 1 * compareValue;
-				else if (b.points < a.points) return -1 * compareValue;
-				else {
-					let dateB = new Date(b.lastUpdate);
-					let dateA = new Date(a.lastUpdate);
-					if (dateB > dateA) return 1;
-					else if (dateB < dateA) return -1;
+		links.reverse().sort((a, b) => {
+			if (b.points > a.points) return 1 * compareValue;
+			else if (b.points < a.points) return -1 * compareValue;
+			else {
+				let dateB = new Date(b.lastUpdate);
+				let dateA = new Date(a.lastUpdate);
+				if (dateB > dateA) return 1;
+				else if (dateB < dateA) return -1;
 
-					return 0;
-				}
-			});
-
-			dispatch(orderLinks(links));
+				return 0;
+			}
 		});
-	};
-}
 
-export function handleLoadLinks() {
-	return (dispatch) => {
-		return linkService.get().then((links) => {
-			dispatch(loadLinks(links.reverse()));
-		});
-	};
-}
+		dispatch(orderLinks(links));
+	});
 
-export function handleAddLink(link) {
-	return (dispatch) => {
-		return linkService.add(link).then((newLink) => {
-			dispatch(addLink(newLink));
-		});
-	};
-}
+export const handleLoadLinks = () => (dispatch) =>
+	linkService.get().then((links) => {
+		dispatch(loadLinks(links.reverse()));
+	});
 
-export function handleRemoveLink(id) {
-	return (dispatch) => {
-		return linkService.remove(id).then(() => {
-			dispatch(removeLink(id));
-		});
-	};
-}
+export const handleAddLink = (link) => (dispatch) =>
+	linkService.add(link).then((newLink) => {
+		dispatch(addLink(newLink));
+	});
 
-export function handleUpvoteLink(id) {
-	return (dispatch) => {
-		return linkService.vote(id, true).then((link) => {
-			dispatch(upvoteLink(id));
-		});
-	};
-}
+export const handleRemoveLink = (id) => (dispatch) =>
+	linkService.remove(id).then(() => {
+		dispatch(removeLink(id));
+	});
 
-export function handleDownvoteLink(id) {
-	return (dispatch) => {
-		return linkService.vote(id, false).then((link) => {
-			dispatch(downvoteLink(id));
-		});
-	};
-}
+export const handleUpvoteLink = (id) => (dispatch) =>
+	linkService.vote(id, true).then(() => {
+		dispatch(upvoteLink(id));
+	});
+
+export const handleDownvoteLink = (id) => (dispatch) =>
+	linkService.vote(id, false).then(() => {
+		dispatch(downvoteLink(id));
+	});
--- src/actions/toasts.js
+++ src/actions/toasts.js
@@ -3,16 +3,12 @@ import { v4 as uuidv4 } from "uuid";
 export const SHOW_TOAST = "SHOW_TOAST";
 export const REMOVE_TOAST = "REMOVE_TOAST";
 
-export function showToast(toast) {
-	return {
-		type: SHOW_TOAST,
-		toast: { ...toast, id: uuidv4() },
-	};
-}
+export const showToast = (toast) => ({
+	type: SHOW_TOAST,
+	toast: { ...toast, id: uuidv4() },
+});
 
-export function removeToast(id) {
-	return {
-		type: REMOVE_TOAST,
-		id,
-	};
-}
+export const removeToast = (id) => ({
+	type: REMOVE_TOAST,
+	id,
+});
